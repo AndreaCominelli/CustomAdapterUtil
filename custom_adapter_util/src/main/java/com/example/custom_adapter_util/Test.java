7f5be3010c9c556dd1c4cb1da7ac8afe26dfb2c8
“Andrea <andreacominelli56@gmail.com>
2019-09-21 11:37:05 +0200

    Committing base files

---
+++ custom_adapter_util/src/main/java/com/example/custom_adapter_util/Test.java
@@ -0,0 +1,15 @@
+package com.example.custom_adapter_util;
+
+public class Test {
+
+    private String text;
+
+    public Test(String text){
+        this.text = text;
+    }
+
+    public void print(){
+        System.out.println(this.text);
+    }
+
+}
